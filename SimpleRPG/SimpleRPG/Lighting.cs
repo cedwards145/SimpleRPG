2b89e560c1537ac386636932dc2cf9af165f38c3
Cam Edwards <cameronedwards145@gmail.com>
2015-06-01 13:11:03 +0100

    Included missing files

---
+++ SimpleRPG/SimpleRPG/Lighting.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace SimpleRPG
+{
+    public class Lighting
+    {
+        private static int time = -1;
+        private static Color lightValue = Color.White;
+        private static int evening, night, morning, afternoon;
+
+        private static float r, g, b, rDelta, gDelta, bDelta;
+
+        private static Color[] lights = { new Color(30, 30, 30), new Color(183, 147, 255),
+                                          new Color(255, 255, 255), new Color(255, 129, 78) };
+
+        public static void update()
+        {
+            time = (time + 1) % 3600;
+
+            if (time == 0)
+                transitionColor(lights[0], lights[1], 900);
+            else if (time == 900)
+                transitionColor(lights[1], lights[2], 900);
+            else if (time == 1800)
+                transitionColor(lights[2], lights[3], 900);
+            else if (time == 2700)
+                transitionColor(lights[3], lights[0], 900);
+
+            r += rDelta;
+            g += gDelta;
+            b += bDelta;
+            lightValue = new Color((int)r, (int)g, (int)b);
+        }
+
+        public static Color getLightValue()
+        {
+            //return lightValue;
+            return lights[2];
+        }
+
+        private static void transitionColor(Color from, Color to, int time)
+        {
+            r = from.R;
+            g = from.G;
+            b = from.B;
+
+            rDelta = (to.R - from.R) / (float)time;
+            gDelta = (to.G - from.G) / (float)time;
+            bDelta = (to.B - from.B) / (float)time;
+        }
+    }
+}
---
+++ SimpleRPG/SimpleRPG/Windows/CraftingWindow.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SimpleRPG.Items;
+
+namespace SimpleRPG.Windows
+{
+    public class CraftingWindow : Window
+    {
+        protected int capacity, filledSoFar = 0;
+        protected Item[] ingredients;
+        protected bool enabled;
+        protected int index = 0;
+
+        public CraftingWindow(Game1 game, Point reqPosition, int reqCapacity, string windowskin)
+            : base(game, reqPosition, 
+                   40 * game.getGraphicsScale(), 
+                   (reqCapacity * 24 + 32) * game.getGraphicsScale(), 
+                   windowskin)
+        {
+            capacity = reqCapacity;
+            ingredients = new Item[capacity];
+        }
+
+        public override void update()
+        {
+            base.update();
+
+            if (enabled)
+            {
+                if (Input.isButtonPressed(ControllerButton.up))
+                {
+                    index = (int)MathHelper.Clamp(index - 1, 0, capacity - 1);
+                }
+                else if (Input.isButtonPressed(ControllerButton.down))
+                {
+                    index = (int)MathHelper.Clamp(index + 1, 0, capacity - 1);
+                }
+            }
+        }
+
+        public override void draw(Microsoft.Xna.Framework.Graphics.SpriteBatch spriteBatch)
+        {
+            base.draw(spriteBatch);
+            int scale = gameRef.getGraphicsScale();
+
+            for (int itemIndex = 0; itemIndex < capacity; itemIndex++)
+            {
+                if (enabled && (itemIndex == index))
+                {
+                    GraphicsHelper.fillRectangle(spriteBatch,
+                                             new Rectangle(location.X + (10 * scale),
+                                                           location.Y + (14 + (24 * itemIndex)) * scale,
+                                                           20 * scale, 20 * scale),
+                                             ColorScheme.selectedTextColor * opacity);
+                }
+
+                GraphicsHelper.drawRectangle(spriteBatch,
+                                             new Rectangle(location.X + (10 * scale),
+                                                           location.Y + (14 + (24 * itemIndex)) * scale,
+                                                           20 * scale, 20 * scale), 
+                                             Color.Black * opacity, scale);
+
+                if (ingredients[itemIndex] != null)
+                {
+                    Texture2D iconSet = ingredients[itemIndex].getIconSet();
+                    int iconIndex = ingredients[itemIndex].getIconIndex();
+
+                    int x = iconIndex % (iconSet.Width / 16);
+                    int y = iconIndex / (iconSet.Width / 16);
+                    Rectangle source = new Rectangle(x * 16, y * 16, 16, 16);
+
+                    spriteBatch.Draw(iconSet, new Rectangle(location.X + (12 * scale),
+                                                           location.Y + (16 + (24 * itemIndex)) * scale,
+                                                           16 * scale, 16 * scale), 
+                                                           source, Color.White * opacity);
+                }
+            }
+        }
+
+        public void setEnabled(bool value)
+        {
+            enabled = value;
+        }
+
+        public bool getEnabled()
+        {
+            return enabled;
+        }
+
+        public Item getItem()
+        {
+            return ingredients[index];
+        }
+
+        public int getIndex()
+        {
+            return index;
+        }
+
+        public string getItemName()
+        {
+            if (ingredients[index] == null)
+                return "item";
+            return ingredients[index].getName();
+        }
+
+        public void resize(int newCapacity)
+        {
+            capacity = newCapacity;
+            ingredients = new Item[capacity];
+            height = (capacity * 24 + 32) * gameRef.getGraphicsScale();
+            index = 0;
+        }
+
+        public bool isFull()
+        {
+            return (filledSoFar >= capacity);
+        }
+
+        public void addItem(Item toAdd)
+        {
+            if (!isFull())
+            {
+                for (int itemIndex = 0; itemIndex < capacity; itemIndex++)
+                {
+                    if (ingredients[itemIndex] == null)
+                    {
+                        ingredients[itemIndex] = toAdd;
+                        break;
+                    }
+                }
+
+                filledSoFar++;
+            }
+        }
+
+        public void removeItem()
+        {
+            if (ingredients[index] != null)
+                filledSoFar--;
+
+            ingredients[index] = null;
+        }
+    }
+}
